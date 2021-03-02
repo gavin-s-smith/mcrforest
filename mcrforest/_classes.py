a7d82f8ffe6278679e2be8777855f11c0c1ead4d
gavin-s-smith <gavin.smith@nottingham.ac.uk>
2021-03-02 12:34:47 +0000

    bug fix

--- mcrforest/_classes.py
+++ mcrforest/_classes.py
@@ -43,8 +43,8 @@ from ._splitter import Splitter
 from ._tree import DepthFirstTreeBuilder
 #from ._tree import BestFirstTreeBuilder
 from ._tree import Tree
-from ._tree import _build_pruned_tree_ccp
-from ._tree import ccp_pruning_path
+#from ._tree import _build_pruned_tree_ccp
+#from ._tree import ccp_pruning_path
 from . import _tree, _splitter, _criterion
 
 __all__ = ["DecisionTreeClassifier",
