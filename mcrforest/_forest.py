f432c6eae3b4a388b39fdac5f1b9aeb86f65c4fa
gavin-s-smith <gavin.smith@nottingham.ac.uk>
2021-07-14 22:04:30 +0100

    bug fix in mcr shap

--- mcrforest/_forest.py
+++ mcrforest/_forest.py
@@ -486,14 +486,14 @@ class BaseForest(MultiOutputMixin, BaseEnsemble, metaclass=ABCMeta):
             
             mm.__class__ = sklrf
             for idx,e in enumerate(mm.estimators_):
-                mm.estimators_[i].tree_ = Wrapper(mm.estimators_[idx].tree_) 
+                mm.estimators_[idx].tree_ = Wrapper(mm.estimators_[idx].tree_) 
             explainerm = shap.TreeExplainer(mm, X, check_additivity=False)
             shap_values_randomm = explainerm.shap_values(X, check_additivity=False)
             rtn_mcr_plus.append( shap_values_randomm[1][:,i] ) 
 
             mm.__class__ = old_class
             for idx,e in enumerate(mm.estimators_):
-                mm.estimators_[i].tree_ = mm.estimators_[idx].tree_._wrapped_obj
+                mm.estimators_[idx].tree_ = mm.estimators_[idx].tree_._wrapped_obj
 
         
         if mcr_plus:
