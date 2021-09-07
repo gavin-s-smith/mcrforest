f110d7e4d6ca1e48052879484f7bd2a8204c85df
gavin-s-smith <gavin.smith@nottingham.ac.uk>
2021-09-07 13:46:22 +0100

    bug fix

--- mcrforest/_forest.py
+++ mcrforest/_forest.py
@@ -502,9 +502,9 @@ class BaseForest(MultiOutputMixin, BaseEnsemble, metaclass=ABCMeta):
                 mm.estimators_[idx].tree_ = Wrapper(mm.estimators_[idx].tree_) 
             explainerm = shap.TreeExplainer(mm, X, check_additivity=False)
             shap_values_randomm = explainerm.shap_values(X, check_additivity=False)
-            print(shap_values_randomm)
-            print(shap_values_randomm.shape)
-            print(var_idx)
+            #print(shap_values_randomm)
+            #print(shap_values_randomm.shape)
+            #print(var_idx)
             rtn_mcr_plus.append( shap_values_randomm[:,var_idx] ) 
 
             mm.__class__ = old_class
