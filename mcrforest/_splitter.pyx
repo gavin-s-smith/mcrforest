a9caf37cfcde7c7dba2a61b10c6e9a46e6970afc
gavin-s-smith <gavin.smith@nottingham.ac.uk>
2021-10-21 12:10:15 +0100

    debug

--- mcrforest/_splitter.pyx
+++ mcrforest/_splitter.pyx
@@ -513,7 +513,7 @@ cdef class BestSplitter(BaseDenseSplitter):
 
 
             # GAVIN - WORK OUT AND STORE SURROGATES
-            if debug == 1:
+            if best.feature == 17: #debug == 1:
                 with gil:
                     print('Best found feature: {}'.format(best.feature))
                     print('Best split: {} - {} - {}'.format(start, best.pos, end))
